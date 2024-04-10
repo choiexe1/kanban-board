c9c413e7f673d4bf656738cac3862ab0db7690ab
Jay Choi <choiexe1@gmail.com>
2024-04-10 22:32:30 +0900

    feat: local auth guard and unit test

---
+++ src/auth/guard/local-auth.guard.spec.ts
@@ -0,0 +1,91 @@
+import { TestingModule, Test } from '@nestjs/testing';
+import { BadRequestException, ExecutionContext } from '@nestjs/common';
+import { LocalAuthGuard } from './local-auth.guard';
+import { createMock } from '@golevelup/ts-jest';
+import { LocalStrategy } from '../strategy/local.strategy';
+import { AuthService } from '../auth.service';
+import { UserService } from 'src/user/user.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from 'src/user/entity/user.entity';
+import { Repository } from 'typeorm';
+import { BcryptService } from 'src/bcrypt/bcrypt.service';
+
+describe('LocalAuthGuard', () => {
+  let localAuthGuard: LocalAuthGuard;
+  let authService: AuthService;
+  const userRepositoryToken = getRepositoryToken(User);
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        LocalAuthGuard,
+        LocalStrategy,
+        AuthService,
+        UserService,
+        {
+          provide: userRepositoryToken,
+          useClass: Repository,
+        },
+        BcryptService,
+      ],
+    }).compile();
+
+    localAuthGuard = module.get<LocalAuthGuard>(LocalAuthGuard);
+    authService = module.get<AuthService>(AuthService);
+  });
+
+  it('should be defined', () => {
+    expect(localAuthGuard).toBeDefined();
+    expect(authService).toBeDefined();
+  });
+
+  describe('canActivate', () => {
+    it('should allow activation for valid credentials', async () => {
+      // Arrange
+      const user = {
+        username: 'test1234',
+        password: 'test1234',
+      };
+
+      const context = createMock<ExecutionContext>();
+
+      context.switchToHttp().getRequest.mockReturnValue({
+        body: user,
+      });
+
+      authService.login = jest
+        .fn()
+        .mockResolvedValue(Object.assign(user, { id: 1 }));
+
+      // Act
+
+      const result = await localAuthGuard.canActivate(context);
+
+      // Assert
+      expect(authService.login).toHaveBeenCalledWith(
+        user.username,
+        user.password,
+      );
+      expect(result).toBe(true);
+    });
+
+    it('should throw BadRequestException for invalid credentials', async () => {
+      // Arrange
+      const user = {
+        username: 'test1234',
+      };
+
+      const context = createMock<ExecutionContext>();
+
+      context.switchToHttp().getRequest.mockReturnValue({
+        body: user,
+      });
+
+      // Act
+      // Assert
+      await expect(
+        async () => await localAuthGuard.canActivate(context),
+      ).rejects.toThrow(BadRequestException);
+    });
+  });
+});
---
+++ src/auth/guard/local-auth.guard.ts
@@ -0,0 +1,33 @@
+import {
+  BadRequestException,
+  ExecutionContext,
+  HttpStatus,
+  Injectable,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { plainToClass } from 'class-transformer';
+import { LoginDto } from '../dto/login.dto';
+import { validate } from 'class-validator';
+
+@Injectable()
+export class LocalAuthGuard extends AuthGuard('local') {
+  async canActivate(context: ExecutionContext) {
+    const request = context.switchToHttp().getRequest<Request>();
+    const body = plainToClass(LoginDto, request.body);
+    const errors = await validate(body);
+
+    const errorMessages = errors.flatMap(({ constraints }) =>
+      Object.values(constraints),
+    );
+
+    if (errorMessages.length > 0) {
+      throw new BadRequestException({
+        message: errorMessages,
+        error: 'Bad Request',
+        statusCode: HttpStatus.BAD_REQUEST,
+      });
+    }
+
+    return super.canActivate(context) as boolean | Promise<boolean>;
+  }
+}
