582b12233477cd1c1eae7ebc5768fbd2ed7db304
Jay Choi <choiexe1@gmail.com>
2024-04-10 18:41:05 +0900

    feat: auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { ConfigurationModule } from './configuration/configuration.module';
 import { DatabaseModule } from './database/database.module';
 import { UserModule } from './user/user.module';
 import { BcryptService } from './bcrypt/bcrypt.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ConfigurationModule, DatabaseModule, UserModule],
+  imports: [ConfigurationModule, DatabaseModule, UserModule, AuthModule],
   providers: [BcryptService],
 })
 export class AppModule {}
