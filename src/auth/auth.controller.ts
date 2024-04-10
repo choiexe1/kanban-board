2ab19448ee5f3c02ac0310e68459fb6c9acf9827
Jay Choi <choiexe1@gmail.com>
2024-04-10 22:42:34 +0900

    fix: CurrentUser Decorator 경로 오류

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,9 @@
 import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guard/local-auth.guard';
-import { CurrentUser } from 'src/shared/currentUser.decorator';
 import { User } from 'src/user/entity/user.entity';
 import { SignUpDto } from './dto/sign-up.dto';
+import { CurrentUser } from 'src/shared/CurrentUser.decorator';
 
 @Controller('auth')
 export class AuthController {
