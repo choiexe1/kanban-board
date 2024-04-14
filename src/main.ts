8cf44b254837fe34916211da136b3db3312d7991
Jay Choi <choiexe1@gmail.com>
2024-04-14 18:11:29 +0900

    feat: 응답 변환 인터셉터

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import { TransformInterceptor } from './shared/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const SERVER_PORT = configService.getOrThrow('SERVER_PORT');
 
+  app.useGlobalInterceptors(new TransformInterceptor());
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
